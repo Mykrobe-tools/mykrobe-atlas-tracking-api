02e42f5457c269427365e0038ac5cc585f11c87c
Giang Nguyen <giang@ebi.ac.uk>
2021-02-22 00:32:31 +0000

    Add session-scoped fixtires

--- openapi_server/test/conftest.py
+++ openapi_server/test/conftest.py
@@ -24,7 +24,7 @@ def client(app):
     return app.app.test_client()
 
 
-@fixture
+@fixture(scope='session')
 def make_request(client):
     def _(path, method, json=None, ensure=False, success_code=200, query_params=None):
         response = client.open(path=path, method=method, json=json, query_string=query_params)
@@ -34,133 +34,133 @@ def make_request(client):
     return _
 
 
-@fixture
+@fixture(scope='session')
 def list_events(make_request):
     def _(sample_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/events', 'GET', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def add_event(make_request):
     def _(sample_id, event, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/events', 'POST', json=event, success_code=201, *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def get_event(make_request):
     def _(sample_id, event_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/events/{event_id}', 'GET', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def delete_event(make_request):
     def _(sample_id, event_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/events/{event_id}', 'DELETE', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def get_file(make_request):
     def _(md5sum, *args, **kwargs):
         return make_request(f'/api/v1/files/{md5sum}', 'GET', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def get_file_of_sample(make_request):
     def _(sample_id, md5sum, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/files/{md5sum}', 'GET', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def add_file(make_request):
     def _(sample_id, file, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/files', 'POST', json=file, success_code=201, *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def list_files(make_request):
     def _(sample_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/files', 'GET', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def delete_file(make_request):
     def _(sample_id, md5sum, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/files/{md5sum}', 'DELETE', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def check_sample(make_request):
     def _(sample_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}', 'HEAD', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def add_or_replace_qc_result(make_request):
     def _(sample_id, qc_result, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/qc-result', 'PUT', json=qc_result, *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def get_qc_result(make_request):
     def _(sample_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/qc-result', 'GET', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def delete_qc_result(make_request):
     def _(sample_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/qc-result', 'DELETE', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def add_or_replace_status(make_request):
     def _(sample_id, status, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/status', 'PUT', json=status, *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def update_status(make_request):
     def _(sample_id, status, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/status', 'PATCH', json=status, *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def get_status(make_request):
     def _(sample_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/status', 'GET', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def delete_status(make_request):
     def _(sample_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}/status', 'DELETE', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def create_sample(make_request):
     def _(sample, *args, **kwargs):
         return make_request(f'/api/v1/samples', 'POST', json=sample, *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def list_samples(make_request):
     def _(experiment_id=None, isolate_id=None, *args, **kwargs):
         return make_request(f'/api/v1/samples', 'GET', query_params={
@@ -171,21 +171,21 @@ def list_samples(make_request):
     return _
 
 
-@fixture
+@fixture(scope='session')
 def get_sample(make_request):
     def _(sample_id, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}', 'GET', *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def patch_sample(make_request):
     def _(sample_id, sample, *args, **kwargs):
         return make_request(f'/api/v1/samples/{sample_id}', 'PATCH', json=sample, *args, **kwargs)
     return _
 
 
-@fixture
+@fixture(scope='session')
 def get_resource(make_request):
     def _(uri, *args, **kwargs):
         return make_request(uri, 'GET', *args, **kwargs)
